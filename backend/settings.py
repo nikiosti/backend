0ecafa07c0d51c624b3f531d44742aaeeafeda4e
nikiosti <yaostin@proton.me>
2024-03-14 21:17:57 +0300

    change: CORS settings

--- backend/settings.py
+++ backend/settings.py
@@ -137,8 +137,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 # CORS
-#! TODO: CHANGE
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
 
 ACCESS_TOKEN_LIFETIME = 25
